c4ec40bf496100bfd8dcbe6c8f36425111ae2306
Caleb Parris <calebparris@live.com>
2021-07-26 00:37:59 -0500

    First day

---
+++ script.js
@@ -0,0 +1,206 @@
+const gameChoice = {
+    game1: {
+        title: "Amazing Frog",
+        pic: ""
+    },
+    game2: {
+        title: "Among Us",
+        pic: ""
+    },
+    game3: {
+        title: "BeamNG.drive",
+        pic: ""
+    },
+    game4: {
+        title: "Bendy and the Ink Machine",
+        pic: ""
+    },
+    game5: {
+        title: "Black Mesa",
+        pic: ""
+    },
+    game6: {
+        title: "Boris and the Dark Survival",
+        pic: ""
+    },
+    game7: {
+        title: "Brick Rigs",
+        pic: ""
+    },
+    game8: {
+        title: "Call of Duty: WWII",
+        pic: ""
+    },
+    game9: {
+        title: "Day of Defeat",
+        pic: ""
+    },
+    game10: {
+        title: "Day of Defeat: Source",
+        pic: ""
+    },
+    game11: {
+        title: "Disassembly 3D",
+        pic: ""
+    },
+    game12: {
+        title: "Fallout 4",
+        pic: ""
+    },
+    game13: {
+        title: "Freddie Fish and the Case of the Missing Kelp Seeds",
+        pic: ""
+    },
+    game14: {
+        title: "Fun with Ragdolls: The Game",
+        pic: ""
+    },
+    game15: {
+        title: "Garry's Mod",
+        pic: ""
+    },
+    game16: {
+        title: "Goat Simulator",
+        pic: ""
+    },
+    game17: {
+        title: "Half-Life",
+        pic: ""
+    },
+    game18: {
+        title: "Half-Life 2",
+        pic: ""
+    },
+    game19: {
+        title: "Half-Life 2: Deathmatch",
+        pic: ""
+    },
+    game20: {
+        title: "Half-Life 2: Episode One",
+        pic: ""
+    },
+    game21: {
+        title: "Half-Life 2: Episode Two",
+        pic: ""
+    },
+    game22: {
+        title: "Half-Life Deathmatch: Source",
+        pic: ""
+    },
+    game23: {
+        title: "Hello Neighbor",
+        pic: ""
+    },
+    game24: {
+        title: "Hello Neighbor 2 Alpha 1.5",
+        pic: ""
+    },
+    game25: {
+        title: "Hello Neighbor Alpha 1",
+        pic: ""
+    },
+    game26: {
+        title: "Hello Neighbor Alpha 2",
+        pic: ""
+    },
+    game27: {
+        title: "Hello Neighbor Alpha 3",
+        pic: ""
+    },
+    game28: {
+        title: "Hello Neighbor: Hide and Seek",
+        pic: ""
+    },
+    game29: {
+        title: "The Henry Stickmin Collection",
+        pic: ""
+    },
+    game30: {
+        title: "Human: Fall Flat",
+        pic: ""
+    },
+    game31: {
+        title: "Kerbal Space Program",
+        pic: ""
+    },
+    game32: {
+        title: "Left 4 Dead 2",
+        pic: ""
+    },
+    game33: {
+        title: "Minecraft",
+        pic: ""
+    },
+    game34: {
+        title: "Naruto Sippuden: Ultimate Ninja Storm 4",
+        pic: ""
+    },
+    game35: {
+        title: "Pajama Sam in No Need to Hide When It's Dark Outside",
+        pic: ""
+    },
+    game36: {
+        title: "People Playground",
+        pic: ""
+    },
+    game37: {
+        title: "Portal",
+        pic: ""
+    },
+    game38: {
+        title: "Portal 2",
+        pic: ""
+    },
+    game39: {
+        title: "Rec Room",
+        pic: ""
+    },
+    game40: {
+        title: "SCP: Secret Laboratory",
+        pic: ""
+    },
+    game41: {
+        title: "SCP strategy Demo",
+        pic: ""
+    },
+    game42: {
+        title: "Simple Planes",
+        pic: ""
+    },
+    game43: {
+        title: "Sonic and Sega All Stars Racing",
+        pic: ""
+    },
+    game44: {
+        title: "Source Filmmaker",
+        pic: ""
+    },
+    game45: {
+        title: "Spy Fox in: Dry Cereal",
+        pic: ""
+    },
+    game46: {
+        title: "Stormworks: Build and Rescue",
+        pic: ""
+    },
+    game47: {
+        title: "Team Fortress 2",
+        pic: ""
+    },
+    game48: {
+        title: "Teardown",
+        pic: ""
+    },
+    game49: {
+        title: "Titanic: The Experience",
+        pic: ""
+    },
+    game50: {
+        title: "Ultimate Epic Battle Simulator",
+        pic: ""
+    },
+    game51: {
+        title: "War Thunder",
+        pic: ""
+    }
+}
